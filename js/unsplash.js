9b369b4a64f15a08c6e43f0a90aae768d329111e
Billie Mead <billie@billiemead.com>
2019-09-27 15:15:38 -0400

    Commit No Images Yet
    Working on API

---
+++ js/unsplash.js
@@ -0,0 +1,12 @@
+// fetch('https://api.unsplash.com/photos/random/?count=10&client_id=35b40e50487743e2de0fb857a76713f765d0115b77b7bb3ea7fa68180a3ad7d8') 
+//    .then(response => response.json())
+//    .then(data => {
+//      data.forEach(function(image, i) {
+//        document.querySelector("#neoscape-slide-" + (i+1)).innerHTML = `
+//          <img src="${image.urls.regular}" alt="" class="unsplash-img">
+//          <p class="unsplash-author-info">
+//            <a href="${image.links.html}">Photo by ${image.user.name}</a> on <a href="https://unsplash.com/" target="_blank">Unsplash</a>
+//          </p>
+//        `;
+//      });
+//    });
